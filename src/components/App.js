fcd0f093ccb1d59b2b6f36309d4e09701a02db3d
ashutosh1pathak <54414308+ashutosh1pathak@users.noreply.github.com>
2020-08-24 02:15:34 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import axios from 'axios';
+
+export function App({ initialData }) {
+  // GitHub usernames: gaearon, sophiebits, sebmarkbage, bvaughn
+
+  const CardList = (props) => (
+    <div>
+      {props.profiles.map(profile => <Card key={profile.id} {...profile} />)}
+    </div>
+  );
+
+  class Card extends React.Component {
+    render() {
+      const profile = this.props;
+      return (
+        <div className="github-profile">
+          <img src={profile.avatar_url} />
+          <div className="info">
+            <div className="name">{profile.name}</div>
+            <div className="company">{profile.company}</div>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  class Form extends React.Component {
+    state = { userName: '' };
+    handleSubmit = async (event) => {
+      event.preventDefault();
+      const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`);
+      this.props.onSubmit(resp.data);
+      this.setState({ userName: '' });
+    };
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.userName}
+            onChange={event => this.setState({ userName: event.target.value })}
+            placeholder="GitHub username"
+            required
+          />
+          <button>Add card</button>
+        </form>
+      );
+    }
+  }
+
+  class Ap extends React.Component {
+    state = {
+      profiles: [],
+    };
+    addNewProfile = (profileData) => {
+      this.setState(prevState => ({
+        profiles: [...prevState.profiles, profileData],
+      }));
+    };
+    render() {
+      return (
+        <div>
+          <div className="header">{this.props.title}</div>
+          <Form onSubmit={this.addNewProfile} />
+          <CardList profiles={this.state.profiles} />
+        </div>
+      );
+    }
+  }
+
+  return (
+    <Ap />
+  );
+}
